c56f31713713fc0e2b39aab8bffdde1c666f0926
Amany889900 <140924529+Amany889900@users.noreply.github.com>
2024-08-08 17:01:32 +0300

    ....

--- angular-iti2/src/app/app.component.ts
+++ angular-iti2/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //Component directive
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular-iti2';
+
+  sayHello():string{
+    return `Hello, ${this.title}`
+  }
 }
