bae5c145196b476f56c03b21b54ec3e8e74238a8
Amany889900 <140924529+Amany889900@users.noreply.github.com>
2024-08-11 12:13:20 +0300

    commit

--- angular-iti2/src/app/Components/order/product-list/product-list.component.ts
+++ angular-iti2/src/app/Components/order/product-list/product-list.component.ts
@@ -18,12 +18,12 @@ constructor(){
     {id:3, name:'mobiles'}
   ];
   this.prdList=[
-   {id:100, name:'LenovoThinkpad laptop', price:100, quantity:1, imgURL:'https://fakeimg.pl/250x100', categoryID:1},
-   {id:200, name:'Apple MackBook laptop', price:200, quantity:0, imgURL:'https://fakeimg.pl/250x100', categoryID:1},
-   {id:300, name:'Lenovo Tab 2', price:300, quantity:10, imgURL:'https://fakeimg.pl/250x100', categoryID:2},
-   {id:400, name:'Samsung Tab', price:400, quantity:2, imgURL:'https://fakeimg.pl/250x100', categoryID:2},
-   {id:500, name:'Samsung Note 10', price:500, quantity:0, imgURL:'https://fakeimg.pl/250x100', categoryID:3},
-   {id:600, name:'Samsung S22', price:600, quantity:1, imgURL:'https://fakeimg.pl/250x100', categoryID:3},
+   {id:100, name:'Lenovo launchpad', price:50000, quantity:1, imgURL:'https://fakeimg.pl/250x100', categoryID:1},
+   {id:200, name:'Apple MackBook', price:60000, quantity:0, imgURL:'https://fakeimg.pl/250x100', categoryID:1},
+   {id:300, name:'Lenovo Tablet', price:5000, quantity:10, imgURL:'https://fakeimg.pl/250x100', categoryID:2},
+   {id:400, name:'Samsung Tablet', price:6500, quantity:2, imgURL:'https://fakeimg.pl/250x100', categoryID:2},
+   {id:500, name:'Samsung Note 10', price:5500, quantity:0, imgURL:'https://fakeimg.pl/250x100', categoryID:3},
+   {id:600, name:'Samsung S22', price:6000, quantity:1, imgURL:'https://fakeimg.pl/250x100', categoryID:3},
   ];
 }
 reduceQuant(selectedItem:IProduct, count:any, prdPrice:number):void{
